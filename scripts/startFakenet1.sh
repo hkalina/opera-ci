9c3c900f6b8bd050a122f732427724dfc0ae1aeb
Jan Kalina <jan.kalina@fantom.foundation>
2022-11-21 19:17:14 +0100

    Fix 1node / 3nodes tests consistency

--- scripts/startFakenet1.sh
+++ scripts/startFakenet1.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+. scripts/helpers.sh
 
 if ./opera version | grep "Version: 1.1.1-"; then
   echo "The opera version is 1.1.1 - skip db.preset"
@@ -9,23 +10,25 @@ else
 fi
 
 echo "Starting Opera with 1/1 fakenet..."
+mkdir -p ./data1
+PORT=$(($PORT_BASE+1))
+RPCP=$(($RPCP_BASE+1))
+WSP=$(($WSP_BASE+1))
 ./opera \
         --fakenet 1/1 \
-        --datadir ./data \
-        --port=5060 \
+        --datadir ./data1 \
+        --port=${PORT} \
         --maxpeers=5 \
         --verbosity=3 \
-        --http \
-        --http.addr=0.0.0.0 \
-        --http.port=18545 \
-        --http.corsdomain="*" \
-        --http.vhosts="*" \
-        --http.api=eth,web3,net,txpool,ftm \
-        $DBPRESET 2>opera.log &
+        --http --http.addr="127.0.0.1" --http.port=${RPCP} --http.corsdomain="*" \
+        --http.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
+        --ws --ws.addr="127.0.0.1" --ws.port=${WSP} --ws.origins="*" \
+        --ws.api="eth,debug,net,admin,web3,personal,txpool,ftm,dag" \
+        $DBPRESET >> opera1.log 2>&1 &
 export OPERAPID=$!
 sleep 3
 echo "Opera started with PID $OPERAPID"
-echo $OPERAPID >opera.pid
-echo "IPC file:"
-ls ./data/opera.ipc
+echo $OPERAPID >opera1.pid
+echo -e "IPC file: "
+ls ./data1/opera.ipc
 
